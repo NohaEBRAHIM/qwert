dd16d2267169fddfecbcc46ad66b6a35f18cc3a0
NohaEBRAHIM <98253328+NohaEBRAHIM@users.noreply.github.com>
2022-02-06 10:54:49 +0300

    noha add comment

--- github251/src/github251/Github251.java
+++ github251/src/github251/Github251.java
@@ -15,7 +15,7 @@ public class Github251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Test code
     }
     
 }
